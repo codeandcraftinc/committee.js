d4020c9074e5ecf8897228a429cefa232cca0ea9
Michael Giuliana <michael.giuliana@gmail.com>
2016-05-24 23:15:58 -0400

    refactor

--- test/functional/index.js
+++ test/functional/index.js
@@ -2,9 +2,10 @@ import request from 'supertest-as-promised'
 import { app } from '../resources'
 
 describe('@todo', () => {
-  xit('@todo', () => {
+  it('@todo', () => {
     return request(app)
       .post('/examples')
+      .set('Content-Type', 'application/json')
       .send()
       .expect(201)
   })
--- test/resources/app.js
+++ test/resources/app.js
@@ -2,18 +2,23 @@ import express from 'express'
 import { json, urlencoded } from 'body-parser'
 import { readFileSync } from 'fs'
 import schema from './schema.json'
-// import { validateRequest } from './committee'
+import { validateRequest } from './committee'
 
 const app = express()
 
 app.use(json())
 app.use(urlencoded({ extended: true }))
-// app.use(validateRequest({ schema: schema }))
+app.use(validateRequest({ schema: schema }))
 app.get('/examples', (req, res) => res.json('list'))
 app.get('/examples/:id', (req, res) => res.json('read'))
 app.post('/examples', (req, res) => res.status(201).json('create'))
 app.patch('/examples/:id', (req, res) => res.json('update'))
 app.delete('/examples/:id', (req, res) => res.json('destroy'))
 app.all('*', (req, res) => res.status(404).send())
+app.use((err, req, res, next) => {
+  console.log(err)
+  console.log(err.stack)
+  next(err)
+})
 
 export default app
