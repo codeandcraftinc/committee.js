aa9037882ccd48fb51d6212d400159d5e3f6e488
Michael Giuliana <michael.giuliana@gmail.com>
2016-05-25 12:00:04 -0400

    implement options

--- test/resources/app.js
+++ test/resources/app.js
@@ -8,17 +8,12 @@ const app = express()
 
 app.use(json())
 app.use(urlencoded({ extended: true }))
-app.use(validateRequest({ schema: schema }))
+app.use(validateRequest({ schema: schema, strict: true }))
 app.get('/examples', (req, res) => res.json('list'))
 app.get('/examples/:id', (req, res) => res.json('read'))
 app.post('/examples', (req, res) => res.status(201).json('create'))
 app.patch('/examples/:id', (req, res) => res.json('update'))
 app.delete('/examples/:id', (req, res) => res.json('destroy'))
 app.all('*', (req, res) => res.status(404).send())
-app.use((err, req, res, next) => {
-  console.log(err)
-  console.log(err.stack)
-  next(err)
-})
 
 export default app
--- test/resources/listen.js
+++ test/resources/listen.js
@@ -1,5 +1,7 @@
 import app from './app'
+
 const port = process.env.PORT || 3000
+
 app.listen(port, console.log.bind(
   console,
   `[committee.js] server listening on port ${port}`
