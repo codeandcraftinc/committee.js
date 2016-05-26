f806bb86aacaad07f9ac26dcc8e7c7ec967cf8c6
Michael Giuliana <michael.giuliana@gmail.com>
2016-05-26 16:01:10 -0400

    Release 0.1.0

--- dist/errors/validation-error.js
+++ dist/errors/validation-error.js
@@ -8,11 +8,21 @@ var _lodash = require('lodash.groupby');
 
 var _lodash2 = _interopRequireDefault(_lodash);
 
+var _lodash3 = require('lodash.sample');
+
+var _lodash4 = _interopRequireDefault(_lodash3);
+
+var _lodash5 = require('lodash.map');
+
+var _lodash6 = _interopRequireDefault(_lodash5);
+
 var _abstractError = require('./abstract-error');
 
 var _abstractError2 = _interopRequireDefault(_abstractError);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const priorities = ['required', 'type', 'pattern', 'format'];
 
 /**
  * @todo
@@ -34,5 +44,18 @@ exports.default = ValidationError; /**
                                     */
 
 ValidationError.generateErrorMessage = function generateErrorMessage(errors) {
-  return '@TODO';
+  if (!errors) return '';
+
+  let groups = (0, _lodash2.default)(errors, 'name');
+  let selectedGroup;
+
+  priorities.some(priority => {
+    selectedGroup = groups[priority];
+    return selectedGroup;
+  });
+  // if there are no 'important' validation errors, just pick one
+  if (!selectedGroup) selectedGroup = (0, _lodash4.default)(groups);
+
+  let message = selectedGroup[0].name + ' error: ' + (0, _lodash6.default)(selectedGroup, 'stack').join(', ');
+  return message;
 };
